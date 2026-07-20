063881ca05025fc796d195732278f04f66ce2649
ntshszl99 <natashas@ktmb.com.my>
2026-07-20 13:10:13 +0800

    Update ModalDetails - BranchDetails. WorkDate, CategoryDetails

--- emujv2Api/obj/Debug/net8.0/emujv2Api.AssemblyInfo.cs
+++ emujv2Api/obj/Debug/net8.0/emujv2Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b3eaa59560907f271683d08a6948af0591e659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d825a114d7d1693608c140a78fcdebc27d72fe5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
