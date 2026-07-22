9999697e77744a8f9726028a80a5fa7511423a6d
ntshszl99 <natashas@ktmb.com.my>
2026-07-22 16:09:27 +0800

    Final Update Excel format for R1

--- emujv2Api/obj/Debug/net8.0/emujv2Api.AssemblyInfo.cs
+++ emujv2Api/obj/Debug/net8.0/emujv2Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3031f41e27824741f00d75566aa0723682dafd9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf30175cfbd3f6503573b96a4c6cb106a0b0e211")]
 [assembly: System.Reflection.AssemblyProductAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("emujv2Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
